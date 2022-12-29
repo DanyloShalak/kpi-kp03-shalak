b8080c9c902825fa1a94cf6fa1e5eefa1ea5becf
DanyloShalak <danylo.shalak@gmail.com>
2022-12-29 15:31:09 +0200

    created-robot-test

--- cli/app.py
+++ cli/app.py
@@ -17,132 +17,194 @@ move_subcatalog = 'move'
 
 @app.command()
 def version():
-    res = httpx.get(base_url + version_catalog)
-    print(res.json())
+    try:
+        res = httpx.get(base_url + version_catalog)
+        print(res.json())
+    except Exception:
+        print('Error occurred')
 
 
 @app.command()
 def dir_get(path: str):
-    res = httpx.get(base_url + directory_catalog + path)
-    print(json.dumps(res.json()))
+    try:
+        res = httpx.get(base_url + directory_catalog + path)
+        print(json.dumps(res.json()))
+    except Exception:
+        print('Error occurred')
 
 
 @app.command()
 def dir_create(path: str, name: str):
-    data = {'directory_path': path,  'directory_name': name}
-    res = httpx.post(base_url + directory_catalog, json=data, headers=headers)
-    print(json.dumps(res.json()))
+    try:
+        data = {'directory_path': path, 'directory_name': name}
+        res = httpx.post(base_url + directory_catalog, json=data, headers=headers)
+        print(json.dumps(res.json()))
+    except Exception:
+        print('Error occurred')
 
 
 @app.command()
 def dir_delete(directory_path: str):
-    res = httpx.delete(base_url + directory_catalog + directory_path)
-    print(json.dumps(res.json()))
+    try:
+        res = httpx.delete(base_url + directory_catalog + directory_path)
+        print(json.dumps(res.json()))
+    except Exception:
+        print('Error occurred')
 
 
 @app.command()
 def dir_move(directory_path: str, destination_path: str):
-    data = {'directory_path': directory_path, 'destination_path': destination_path}
-    res = httpx.post(base_url + directory_catalog + move_subcatalog, json=data, headers=headers)
-    print(json.dumps(res.json()))
+    try:
+        data = {'directory_path': directory_path, 'destination_path': destination_path}
+        res = httpx.post(base_url + directory_catalog + move_subcatalog, json=data, headers=headers)
+        print(json.dumps(res.json()))
+    except Exception:
+        print('Error occurred')
 
 
 @app.command()
 def log_create(path: str, file_name: str):
-    data = {'directory_path': path,  'file_name': file_name}
-    res = httpx.post(base_url + logfile_catalog, json=data, headers=headers)
-    print(json.dumps(res.json()))
+    try:
+        data = {'directory_path': path, 'file_name': file_name}
+        res = httpx.post(base_url + logfile_catalog, json=data, headers=headers)
+        print(json.dumps(res.json()))
+    except Exception:
+        print('Error occurred')
 
 
 @app.command()
 def log_log(file_path: str, log_line: str):
-    data = {'log_file_path': file_path, 'log_line': log_line}
-    res = httpx.post(base_url + logfile_catalog + 'log', json=data, headers=headers)
-    print(json.dumps(res.json()))
+    try:
+        data = {'log_file_path': file_path, 'log_line': log_line}
+        res = httpx.post(base_url + logfile_catalog + 'log', json=data, headers=headers)
+        print(json.dumps(res.json()))
+    except Exception:
+        print('Error occurred')
 
 
 @app.command()
 def log_get(file_path: str):
-    res = httpx.get(base_url + logfile_catalog + file_path)
-    print(json.dumps(res.json()))
+    try:
+        res = httpx.get(base_url + logfile_catalog + file_path)
+        print(json.dumps(res.json()))
+    except Exception:
+        print('Error occurred')
 
 
 @app.command()
 def log_move(file_path: str, destination_path: str):
-    data = {'log_file_path': file_path, 'destination_path': destination_path}
-    res = httpx.post(base_url + logfile_catalog + move_subcatalog, json=data, headers=headers)
-    print(json.dumps(res.json()))
+    try:
+        data = {'log_file_path': file_path, 'destination_path': destination_path}
+        res = httpx.post(base_url + logfile_catalog + move_subcatalog, json=data, headers=headers)
+        print(json.dumps(res.json()))
+    except Exception:
+        print('Error occurred')
 
 
 @app.command()
 def log_delete(file_path: str):
-    res = httpx.delete(base_url + logfile_catalog + file_path)
-    print(json.dumps(res.json()))
+    try:
+        res = httpx.delete(base_url + logfile_catalog + file_path)
+        print(json.dumps(res.json()))
+    except Exception:
+        print('Error occurred')
 
 
 @app.command()
 def bin_create(path: str, file_name: str, content: str):
-    data = {'directory_path': path,  'file_name': file_name, 'content': content}
-    res = httpx.post(base_url + binaryfile_catalog, json=data, headers=headers)
-    print(json.dumps(res.json()))
+    try:
+        data = {'directory_path': path, 'file_name': file_name, 'content': content}
+        res = httpx.post(base_url + binaryfile_catalog, json=data, headers=headers)
+        print(json.dumps(res.json()))
+    except Exception:
+        print('Error occurred')
 
 
 @app.command()
 def bin_get(file_path: str):
-    res = httpx.get(base_url + binaryfile_catalog + file_path)
-    print(json.dumps(res.json()))
+    try:
+        res = httpx.get(base_url + binaryfile_catalog + file_path)
+        print(json.dumps(res.json()))
+    except Exception:
+        print('Error occurred')
 
 
 @app.command()
 def bin_delete(file_path: str):
-    res = httpx.delete(base_url + binaryfile_catalog + file_path)
-    print(json.dumps(res.json()))
+    try:
+        res = httpx.delete(base_url + binaryfile_catalog + file_path)
+        print(json.dumps(res.json()))
+    except Exception:
+        print('Error occurred')
 
 
 @app.command()
 def bin_move(file_path: str, destination_path: str):
-    data = {'binary_file_path': file_path, 'destination_path': destination_path}
-    res = httpx.post(base_url + binaryfile_catalog + move_subcatalog, json=data, headers=headers)
-    print(json.dumps(res.json()))
+    try:
+        data = {'binary_file_path': file_path, 'destination_path': destination_path}
+        res = httpx.post(base_url + binaryfile_catalog + move_subcatalog, json=data, headers=headers)
+        print(json.dumps(res.json()))
+    except Exception:
+        print('Error occurred')
 
 
 @app.command()
 def buff_create(path: str, file_name: str):
-    data = {'directory_path': path,  'file_name': file_name}
-    res = httpx.post(base_url + bufferfile_catalog, json=data, headers=headers)
-    print(json.dumps(res.json()))
+    try:
+        data = {'directory_path': path, 'file_name': file_name}
+        res = httpx.post(base_url + bufferfile_catalog, json=data, headers=headers)
+        print(json.dumps(res.json()))
+    except Exception:
+        print('Error occurred')
 
 
 @app.command()
 def buff_delete(file_path: str):
-    res = httpx.delete(base_url + bufferfile_catalog + file_path)
-    print(json.dumps(res.json()))
+    try:
+        res = httpx.delete(base_url + bufferfile_catalog + file_path)
+        print(json.dumps(res.json()))
+    except Exception:
+        print('Error occurred')
 
 
 @app.command()
 def buff_get(file_path: str):
-    res = httpx.get(base_url + bufferfile_catalog + file_path)
-    print(json.dumps(res.json()))
+    try:
+        res = httpx.get(base_url + bufferfile_catalog + file_path)
+        print(json.dumps(res.json()))
+    except Exception:
+        print('Error occurred')
+
 
 
 @app.command()
 def buff_move(file_path: str, destination_path: str):
-    data = {'buffer_file_path': file_path, 'destination_path': destination_path}
-    res = httpx.post(base_url + bufferfile_catalog + move_subcatalog, json=data, headers=headers)
-    print(json.dumps(res.json()))
+    try:
+        data = {'buffer_file_path': file_path, 'destination_path': destination_path}
+        res = httpx.post(base_url + bufferfile_catalog + move_subcatalog, json=data, headers=headers)
+        print(json.dumps(res.json()))
+    except Exception:
+        print('Error occurred')
 
 
 @app.command()
 def buff_push(file_path: str, push_element: str):
-    data = {'buffer_file_path': file_path, 'push_element': push_element}
-    res = httpx.post(base_url + bufferfile_catalog + 'push', json=data, headers=headers)
-    print(json.dumps(res.json()))
+    try:
+        data = {'buffer_file_path': file_path, 'push_element': push_element}
+        res = httpx.post(base_url + bufferfile_catalog + 'push', json=data, headers=headers)
+        print(json.dumps(res.json()))
+    except Exception:
+        print('Error occurred')
 
 
 @app.command()
 def buff_pop(file_path: str):
-    res = httpx.get(base_url + bufferfile_catalog + 'pop/' + file_path)
-    print(json.dumps(res.json()))
+    try:
+        res = httpx.get(base_url + bufferfile_catalog + 'pop/' + file_path)
+        print(json.dumps(res.json()))
+    except Exception:
+        print('Error occurred')
+
 
 
 
